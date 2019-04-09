fc60b5d5131b6514b0e032cbf8c4458da9f81232
chrisob194 <46025769+chrisob194@users.noreply.github.com>
2019-04-09 11:09:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import nxppy
 import ndef
-
+'''
 def code_finder(ndef_string,len_code):
     index = ndef_string.index('Text',17)
     code_start = index+6
     code_end = index+6+len_code
     code = ndef_string[code_start:code_end]
     return code
-
+'''
 
 # Instantiate reader
 mifare = nxppy.Mifare()
@@ -21,7 +21,10 @@ ndef_data = mifare.read_ndef()
 # Parse NDEF data
 ndef_records = list(ndef.message_decoder(ndef_data))
 print(ndef_records)
-#print(type(ndef_records))
+code = ndef_records.text
+print(code)
+'''
 stringa = str(ndef_records[0])
 code = code_finder(stringa,5)
 print(code)
+'''
