ba74e9332b38bb3dde971386005080a0907da39a
chrisob194 <46025769+chrisob194@users.noreply.github.com>
2019-04-09 11:28:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import nxppy
 import ndef
-'''
+
 def code_finder(ndef_string,len_code):
     index = ndef_string.index('Text',17)
     code_start = index+6
     code_end = index+6+len_code
     code = ndef_string[code_start:code_end]
     return code
-'''
 
-# Instantiate reader
-mifare = nxppy.Mifare()
+def main():
+    # Instantiate reader
+    mifare = nxppy.Mifare()
+    while True:
+        try:
+            # Select tag
+            uid = mifare.select()
+            # Read NDEF data
+            ndef_data = mifare.read_ndef()
+            # Parse NDEF data
+            ndef_records = list(ndef.message_decoder(ndef_data))
+            print(ndef_records)
+            stringa = str(ndef_records[0])
+            code = code_finder(stringa,5)
+            print(code)
+        except nxppy.SelectError:
+        # SelectError is raised if no card is in the field.
+        pass
 
-# Select tag
-uid = mifare.select()
+        time.sleep(3)
 
-# Read NDEF data
-ndef_data = mifare.read_ndef()
-
-# Parse NDEF data
-ndef_records = ndef.message_decoder(ndef_data)
-print(type(ndef_records))
-#code = ndef_records.text
-#print(code)
-'''
-stringa = str(ndef_records[0])
-code = code_finder(stringa,5)
-print(code)
-'''
+if __name__ == '__main__':
+    main()
