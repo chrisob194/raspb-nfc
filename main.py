2c448c4aca6383a6e18f89c07c9023cec5e1d153
chrisob194 <46025769+chrisob194@users.noreply.github.com>
2019-04-09 12:03:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
             print(code)
             current_time = time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime())
             print(current_time)
-            registro.update(code:current_time)
+            registro[code] = current_time
         except nxppy.SelectError:
             # SelectError is raised if no card is in the field.
             pass
